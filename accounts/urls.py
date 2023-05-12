0415ddf5c8fbbb5e6b6c7bedd55102935df4f43e
fanta95 <fantael95@gmail.com>
2023-05-12 11:01:10 +0900

    accounts views작성

--- accounts/urls.py
+++ accounts/urls.py
@@ -16,12 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
-
+from . import views
 
 
 
 app_name = 'accounts'
 
 urlpatterns = [
-
+    path('', views.index, name='index'),
+    path('signup/', views.signup, name='signup'),
+    path('delete/', views.delete, name='delete'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('mypage/<str:username>', views.profile, name='mypage'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as auth_login, logout as auth_logout
+from .forms import CustomUserCreationForm
 
 # Create your views here.
+def login(request):
+    if request.user.is_authenticated:
+        return redirect('dinings:index')
+    
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('dinings:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/login.html', context)
+
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect('dinings:index')
+
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST, request.FILES)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('dinings:index')
+    else:
+        form = CustomUserCreationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/signup.html', context)
