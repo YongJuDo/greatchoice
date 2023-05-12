b2fdc9efae1fa78de8f57dba2e5ff71849d50fc4
fanta95 <fantael95@gmail.com>
2023-05-12 15:10:36 +0900

    review 작성 폼, view,url 작성

--- reviews/urls.py
+++ reviews/urls.py
@@ -25,4 +25,10 @@ app_name = 'reviews'
 urlpatterns = [
     path('', views.index, name='index'),
     path('category/', views.category, name='category'),
+    path('detail/<int:product_pk>', views.detail, name='detail'),
+    path('product_create/', views.product_create, name='product_create'),
+    path('detail/<int:product_pk>/review_create/', views.review_create, name='review_create'),
+    path('detail/<int:product_pk>/delete/<int:review_pk>/', views.delete, name='delete'),
+    path('update/<int:product_pk>/', views.product_update, name='product_update'),
+    path('detail/<int:product_pk>/update/<int:review_pk>/', views.review_update, name='review_update'),
 ]
