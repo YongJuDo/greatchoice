aff1d3f4f6d82fddaf96ef3cfbbe14fe218fab68
fanta95 <fantael95@gmail.com>
2023-05-12 11:11:57 +0900

    account views 수정

--- accounts/urls.py
+++ accounts/urls.py
@@ -23,10 +23,9 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('signup/', views.signup, name='signup'),
     path('delete/', views.delete, name='delete'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
-    path('mypage/<str:username>', views.profile, name='mypage'),
+    path('mypage/<str:username>', views.mypage, name='mypage'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,17 +2,18 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login as auth_login, logout as auth_logout
 from .forms import CustomUserCreationForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def login(request):
     if request.user.is_authenticated:
-        return redirect('dinings:index')
+        return redirect('reviews:index')
     
     if request.method == 'POST':
         form = AuthenticationForm(request, request.POST)
         if form.is_valid():
             auth_login(request, form.get_user())
-            return redirect('dinings:index')
+            return redirect('reviews:index')
     else:
         form = AuthenticationForm()
     context = {
@@ -22,17 +23,31 @@ def login(request):
 
 def signup(request):
     if request.user.is_authenticated:
-        return redirect('dinings:index')
+        return redirect('reviews:index')
 
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST, request.FILES)
         if form.is_valid():
             user = form.save()
             auth_login(request, user)
-            return redirect('dinings:index')
+            return redirect('reviews:index')
     else:
         form = CustomUserCreationForm()
     context = {
         'form': form,
     }
     return render(request, 'accounts/signup.html', context)
+
+@login_required
+def logout(request):
+    auth_logout(request)
+    return redirect('reviews:index')
+
+@login_required
+def delete(request):
+    request.user.delete()
+    auth_logout(request)
+    return redirect('reviews:index')
+
+def mypage(request):
+    return render(request, 'accounts/mypage.html')
