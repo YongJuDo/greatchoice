1821089d63ac1527976b5ad68ca7adeb87a863ab
YongJuDo <qud6330@naver.com>
2023-05-15 09:55:44 +0900

    update category & search

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,22 +1,4 @@
-"""greatchoice URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls.static import static
-from django.conf import settings
+from django.urls import path
 from . import views
 
 
@@ -24,7 +6,8 @@ app_name = 'reviews'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('category/', views.category, name='category'),
+    path('category/<str:category_type>', views.category, name='category'),
+    path('search/', views.search, name='search'),
     path('detail/<int:product_pk>', views.detail, name='detail'),
     path('product_create/', views.product_create, name='product_create'),
     path('detail/<int:product_pk>/review_create/', views.review_create, name='review_create'),
